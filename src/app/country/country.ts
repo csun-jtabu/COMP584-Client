273b688b6b6ac288a2aed90b6c74d2f5f89f74c0
jst83366 <jst83366@ECS1538-01>
2025-10-17 18:18:51 -0700

    10-17-2025 Classwork

--- src/app/country/country.ts
+++ src/app/country/country.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CountryData } from './country-data';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country.html',
   styleUrl: './country.scss'
 })
 export class Country {
+  countries: CountryData[] = [];
 
+  constructor(http: HttpClient)
+  {
+    http.get<CountryData[]>(environment.apiUrl + "api/countries").subscribe(result => {
+      this.countries = result;
+    });
+  }
 }
