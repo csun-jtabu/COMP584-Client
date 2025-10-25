36be7577b7bed02a10b954788af94a96b3b4966a
jst83366 <jst83366@ECS1538-01>
2025-10-24 18:13:49 -0700

    10/24/2025 Classwork

--- src/app/country-population/country-population.ts
+++ src/app/country-population/country-population.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopulationData } from './population-data';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.scss'
 })
-export class CountryPopulation {
-  country!: PopulationData;
 
-  constructor(http: HttpClient)
+export class CountryPopulation implements OnInit {
+  CountryPopulation!: PopulationData;
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute)
   {
-    
-    http.get<PopulationData>(environment.apiUrl + "api/Countries/population/26").subscribe(result => {
-      this.country = result;
+  }
+
+  ngOnInit(): void {
+    let idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get<PopulationData>(`${environment.apiUrl}api/Countries/population/${idParam}`).subscribe(result => {
+      this.CountryPopulation = result;
     });
+
   }
 }
